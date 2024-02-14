eab1c19e50c0896c7e578100daeb63795e6724cc
Hardik Shali <hardik.s@accubits.com>
2024-02-14 19:49:04 +0530

    structured the theme according to the JSX and React

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const footer = () => {
+    return (
+        <footer class="py-6 text-center lg:ml-auto footer-section max-w-content xl:max-2xl:max-w-50rem">
+            <p class="">
+                Copyright by
+                <a href="#" class="transition-colors">@domain.com</a>
+            </p>
+        </footer>
+    )
+}
+
+export default footer
