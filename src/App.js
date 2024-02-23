6dbe93dbcbaaaecdf17e46bb45a88c00413d530f
Hardik Shali <hardik.s@accubits.com>
2024-02-23 11:51:47 +0530

    removed nodejs

--- src/App.js
+++ src/App.js
@@ -20,29 +20,29 @@ function App() {
     setIsMenuOpen(!isMenuOpen);
   }
 
-  // Function to record visitor data
-  const recordVisitorData = async () => {
-    try {
-      // Make a POST request to the server to record visitor data
-      await axios.post('http://localhost:3001/record-visitor', {
-        // Pass any necessary data in the request body
-        // For example, you might include IP address, email, etc.
-      });
-      console.log('Visitor data recorded successfully.');
-    } catch (error) {
-      console.error('Error recording visitor data:', error);
-    }
-  };
+  // // Function to record visitor data
+  // const recordVisitorData = async () => {
+  //   try {
+  //     // Make a POST request to the server to record visitor data
+  //     await axios.post('http://localhost:3001/record-visitor', {
+  //       // Pass any necessary data in the request body
+  //       // For example, you might include IP address, email, etc.
+  //     });
+  //     console.log('Visitor data recorded successfully.');
+  //   } catch (error) {
+  //     console.error('Error recording visitor data:', error);
+  //   }
+  // };
 
-  // Call the function to record visitor data when the component mounts
-  useEffect(() => {
-    recordVisitorData();
-  }, []); // Empty dependency array ensures the effect runs only once when the component mounts
+  // // Call the function to record visitor data when the component mounts
+  // useEffect(() => {
+  //   recordVisitorData();
+  // }, []); // Empty dependency array ensures the effect runs only once when the component mounts
 
-  // Call the function to record visitor data whenever the component re-renders (on each page load or navigation)
-  useEffect(() => {
-    recordVisitorData();
-  });
+  // // Call the function to record visitor data whenever the component re-renders (on each page load or navigation)
+  // useEffect(() => {
+  //   recordVisitorData();
+  // });
   return (
     <div className="App">
       <div class="relative pt-10 minfo__app max-xl:pt-20">
